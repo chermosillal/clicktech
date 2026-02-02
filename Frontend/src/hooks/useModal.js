ca3ee9fd3de2db2159a00de28cb0d4a5201b8d4c
Cesar Hermosilla <cesar.hermosilla@gmail.com>
2026-02-02 03:06:54 -0300

    espacio

--- Frontend/src/hooks/useModal.js
+++ Frontend/src/hooks/useModal.js
@@ -1,7 +1,5 @@
-
 import { useContext } from 'react';
 import ModalContext from '../context/ModalContextDef';
-
 export default function useModal() {
   const {modal, openModal, closeModal, extra} = useContext(ModalContext);
   return {modal, openModal, closeModal, extra};
